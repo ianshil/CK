82197845cd560e912db313ef09cad0e94aa8af22
ianshill <ian.shillito@anu.edu.au>
2024-06-27 11:24:40 +1000

    Delete old

--- theories/GHC/CKH_export.v
+++
@@ -1,5 +0,0 @@
-Require Export CKH.
-Require Export enum.
-Require Export logic.
-Require Export properties.
-Require Export Lindenbaum_lem.
--- theories/Kripke/kripke_export.v
+++
@@ -1,3 +0,0 @@
-Require Export kripke_sem.
-Require Export soundness.
-Require Export completeness.
