c98942b874693095fa201ad82d92e332b386e8fb
ianshill <ian.shillito@anu.edu.au>
2024-07-01 16:59:40 +1000

    Update sufficient props

--- theories/Completeness_th/IK_th_completeness.v
+++ theories/Completeness_th/IK_th_completeness.v
@@ -13,19 +13,35 @@ Section IK_completeness.
 
 Definition is_Nd := (fun x => Nd = x).
 
-Lemma CF_Nd : Nd_frame (CF is_Nd).
+Lemma CF_suff_Nd : suff_Nd_frame (CF is_Nd).
 Proof.
-intros w Hw.
-apply cmreach_expl ; auto.
-unfold cmreach ; cbn. split ; intros A HA.
-- pose (Hw _ (ireach_refl w)). destruct m.
-  apply Closed. eapply MP. apply EFQ. eapply MP.
-  apply Ax ; right ; left ; reflexivity. apply Id. apply H0.
-  unfold th ; unfold cexpl ; cbn ; unfold AllForm ; auto.
+intros w (H & H0).
+apply cworld_prf_irrel.
+apply Extensionality_Ensembles ; split ; intros A HA ; unfold In, head in * ; cbn in *.
 - unfold AllForm ; auto.
+- apply (Closed is_Nd) ; auto. eapply MP. apply EFQ.
+  eapply MP. apply Ax. right. left. reflexivity. apply Id.
+  apply H0 ; auto.
 Qed.
 
-Theorem Strong_Completeness : forall Γ φ,
+Lemma CF_Nd : Nd_frame (CF is_Nd).
+Proof.
+apply suff_impl_Nd. apply CF_suff_Nd.
+Qed.
+
+Theorem suff_suff_suff_IK_Strong_Completeness : forall Γ φ,
+    loc_conseq (fun F => suff_Cd_frame F /\ suff_Idb_frame F /\ suff_Nd_frame F) Γ φ -> IKH_prv Γ φ.
+Proof.
+intros. apply more_AdAx_more_prv with (AdAxCdIdb is_Nd).
+- intros A HA. destruct HA ; auto.
+- apply Strong_Completeness with (ClassF:= fun F => suff_Cd_frame F /\ suff_Idb_frame F /\ suff_Nd_frame F) ; auto.
+  repeat split ; auto.
+  + apply strong_is_suff_Cd. apply CF_strong_Cd_weak_Idb.
+  + apply CF_suff_Idb.
+  + apply CF_suff_Nd.
+Qed.
+
+Theorem IK_Strong_Completeness : forall Γ φ,
     loc_conseq (fun F => Cd_frame F /\ Idb_frame F /\ Nd_frame F) Γ φ -> IKH_prv Γ φ.
 Proof.
 intros. apply more_AdAx_more_prv with (AdAxCdIdb is_Nd).
